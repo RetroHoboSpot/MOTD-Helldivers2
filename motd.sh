c6c90dca75ef94b8dceca230fbdb82cf9c9b2281
RetroHoboSpot <160140131+RetroHoboSpot@users.noreply.github.com>
2024-04-07 13:58:05 +0800

    motd.sh
    script for doing the thing

---
+++ motd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Path to the MOTD text file
+MOTD_FILE="/etc/motd.txt"
+
+# Get the number of lines in the MOTD file
+num_lines=$(wc -l < "$MOTD_FILE")
+
+# Generate a random number within the range of the number of lines
+random_num=$((RANDOM % num_lines + 1))
+
+# Get the random MOTD message
+motd_message=$(sed -n "${random_num}p" "$MOTD_FILE")
+
+# Display the MOTD message
+echo "$motd_message"
